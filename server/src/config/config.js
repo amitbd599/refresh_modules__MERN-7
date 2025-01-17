966df370e46e2db777e571157b80999cec1861f8
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-17 09:37:35 +0600

    add atlas

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,7 @@
 export const PORT = 5000;
-export const DATABASE = "mongodb://127.0.0.1:27017/refresh_modules";
+// export const DATABASE = "mongodb://127.0.0.1:27017/refresh_modules";
+export const DATABASE =
+  "mongodb+srv://demo:Fa.95.CD2QetvMh@cluster0.fsp0qs4.mongodb.net/refresh_modules?retryWrites=true&w=majority&appName=Cluster0";
 export const JWT_KEY = "ABC12341241234";
 export const JWT_EXPIRE_TIME = "30d"; //30 * 24 * 60 * 60
 export const Cookie_EXPIRE_TIME = "2592000"; //30 * 24 * 60 * 60
